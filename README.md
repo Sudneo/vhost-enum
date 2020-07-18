746e264c387a2b06eccc507c95baaa985f09e1cb
sudneo <mucci.daniele@gmail.com>
2020-07-18 12:04:31 +0300

    Adds the whole content.

---
+++ README.md
@@ -0,0 +1,83 @@
+## Vhost-enum
+
+This is a simple Python script (requires Python 3.6+) used to enumerate virtualhosts.
+The main use-case is during CTFs or HackTheBox machines where different sites are served based on the virtualhosts.
+
+For example:
+
+```
+# Serves default page
+www.example.com
+# Serves hidden site
+blog.example.com
+```
+
+Usually, in order to detect such sites it is necessary to add every host to the `/etc/hosts` file, or do some scripting
+with `curl` (using the `--resolve` option).
+
+This script makes it slightly simpler.
+
+## How does it work?
+
+The help of the script says it all:
+
+```bash
+python3 vhost-enum.py -h                                                     
+usage: vhost-enum.py [-h] [-w WORDLIST] [-s] -i IP [-p PORT] -d DOMAIN
+                     [-b BASELINE] [-t THREADS]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -w WORDLIST, --wordlist WORDLIST
+                        The wordlist to use
+  -s, --tls             Use HTTPs
+  -i IP, --ip IP        The ip of the host
+  -p PORT, --port PORT  A custom port to use
+  -d DOMAIN, --domain DOMAIN
+                        The domain to use
+  -b BASELINE, --baseline BASELINE
+                        The baseline subdomain to use
+  -t THREADS, --threads THREADS
+                        Number of threads to use
+```
+
+The `-w` options is to specify a newline separated list of words to use a subdomains. This can include also
+sub-subdomains (e.g., `dev.test`).
+
+The `-s` option is used to specify whether we want TLS or not. If the options is set, the requests will be made to port
+`443`, otherwise they will go to port `80`, unless `-p` is specified.
+
+The `-i` option is used to specify the IP to use for the DNS resolution (e.g., `10.10.10.1`).
+
+The `-p` option is used to specify a custom port to use, for example `8080`.
+
+Please make sure that `-s` and `-p` options are used sensibly (requesting HTTPs to HTTP port or vice versa will obviously fail).
+
+The `-d` option is used to specify the main domain name to use (e.g., `machine.htb`).
+
+The `-b` option is used to specify the baseline subdomain, so one domain that we already know exists (defaults to `www`).
+
+The `-t` option is used to specify how many threads to use as workers (to make requests).
+
+The login is the following:
+
+* All requests will skip DNS resolution, that will be overridden with the IP specified. 
+* A request to baseline.domain:port (or `80`/`443` depending on `-s` if `-p` is not specified) will be performed. The
+length and hash of the response content will be saved and used to compare the response coming from other virtualhosts.
+* A request to word.domain:port (or `80`/`443` depending on `-s` if `-p` is not specified) will be performed, for every `word` in
+the wordlist.
+
+At this point, the script makes the following choices:
+
+* If the request returned 200, but with the same length or hash as the baseline, this is considered to be the same page
+and therefore the corresponding `word` won't be validated (this is the case for a `default` backend in Haproxy for example,
+that will respond to every request that doesn't match other ACLs). The reason why the length is also used is to try to catch
+pages where some small content changes (maybe a timestamp), but the page is basically the same (so hash won't match).
+* If the request returned 200 and the hash and length of the response are different from the baseline, the virtualhost will be
+added to the discovered ones and will be reported.
+
+### Note about the wordlists
+
+The wordlist included by default with this repo is for test purposes only (still faster to dump ideas there that in hosts file). 
+In order to find appropriate lists, it might be possible to use [Cewl](https://github.com/digininja/CeWL) o generate
+a custom one or use some generic list from [seclists](https://github.com/danielmiessler/SecLists/blob/master/Discovery/DNS/bitquark-subdomains-top100000.txt) or similar projects.
---
+++ vhost-enum.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+import argparse
+import hashlib
+import queue
+import requests
+import socket
+import time
+import threading
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+dns_cache = {}
+prv_getaddrinfo = socket.getaddrinfo
+
+
+# Override default socket.getaddrinfo() and pass ip instead of host
+# if override is detected
+def new_getaddrinfo(*args):
+    if args[0] in dns_cache:
+        return prv_getaddrinfo(dns_cache[args[0]], *args[1:])
+    else:
+        return prv_getaddrinfo(*args)
+
+
+socket.getaddrinfo = new_getaddrinfo
+
+
+# Capture a dict of hostname and their IPs to override with
+def override_dns(domain, ip):
+    dns_cache[domain] = ip
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-w", "--wordlist", help="The wordlist to use", default="./vhosts.txt", required=False)
+    parser.add_argument("-s", "--tls", action='store_true', help="Use HTTPs")
+    parser.add_argument("-i", "--ip", help="The ip of the host", required=True)
+    parser.add_argument("-p", "--port", help="A custom port to use")
+    parser.add_argument("-d", "--domain", help="The domain to use", required=True)
+    parser.add_argument('-b', "--baseline", help="The baseline subdomain to use", default="www")
+    parser.add_argument('-t', "--threads", default=1, help="Number of threads to use", type=int)
+    return parser.parse_args()
+
+
+def get_site(ip, host, subdomain, tls, custom_port, http_session):
+    url = f"{subdomain}.{host}"
+    override_dns(url, ip)
+    if tls:
+        if custom_port is None:
+            url = f"https://{url}:443"
+        else:
+            url = f"https://{url}:{custom_port}"
+    else:
+        if custom_port is None:
+            url = f"http://{url}:80"
+        else:
+            url = f"http://{url}:{custom_port}"
+    try:
+        response = http_session.get(url, verify=False)
+    except requests.exceptions.SSLError:
+        print(f"\t[-] {url} was requested but SSL error occurred (is the site using TLS?).")
+        return None, None
+    except ConnectionError:
+        print(f"\t[-] Failed to connect to {ip}.")
+        return None, None
+    if response.status_code == 200:
+        length = len(response.content)
+        hash_object = hashlib.sha256(response.content)
+        hash_value = hash_object.hexdigest()
+        return length, hash_value
+    else:
+        print(f"\t[-] Request to {url} failed.")
+        return None, None
+
+
+def get_wordlist_queue(wordlist_file):
+    print("[+] Generating wordlist...")
+    words_queue = queue.Queue()
+    try:
+        with open(wordlist_file) as fp:
+            content = fp.read()
+            words = content.split('\n')
+    except FileNotFoundError:
+        print(f"[-] File {wordlist_file} does not exist. Aborting.")
+        exit(1)
+    # Removes empty lines
+    words = filter(None, words)
+    # Removes duplicates
+    words = set(words)
+    for w in words:
+        words_queue.put(w)
+    print(f"[+] Loaded {words_queue.qsize()} words.")
+    return words_queue
+
+
+def consume_words(wordlist_queue, ip, port, tls, domain, l_baseline, h_baseline, result_list):
+    http_session = requests.session()
+    while not wordlist_queue.empty():
+        word = wordlist_queue.get()
+        length, digest = get_site(ip, domain, word.rstrip('\n'), tls, port, http_session)
+        if length is not None and digest is not None:
+            if length == l_baseline or digest == h_baseline:
+                print(f"\t[!] {word}.{domain} returns 200, but the content seems to be the same as the one of main site.")
+            else:
+                print(f"\t[+] {word}.{domain} returns 200 and seems a different site")
+                result_list.append(f"{word}.{domain}")
+
+
+def main():
+    args = get_args()
+    wordlist = args.wordlist
+    tls = args.tls
+    ip = args.ip
+    port = args.port
+    domain = args.domain
+    baseline = args.baseline
+    threads = args.threads
+    wordlist_queue = get_wordlist_queue(wordlist)
+    l_baseline, h_baseline = get_site(ip, domain, baseline, tls, port, http_session=requests.session())
+    if l_baseline is None or h_baseline is None:
+        print(f"[-] Establishing baseline failed. Make sure that {baseline}.{domain} exists "
+              f"and that you are using the correct port.")
+        exit(1)
+    print(f"[+] Established baseline: {baseline}.{domain} returns a "
+          f"page of {l_baseline} bytes and with hash {h_baseline}.")
+    confirmed = list()
+    threads_list = list()
+    print(f"[+] Spawning {threads} thread(s)...")
+    timestamp_start = time.time()
+    for i in range(threads):
+        worker = threading.Thread(target=consume_words, args=(wordlist_queue, ip, port, tls, domain, l_baseline,
+                                                              h_baseline, confirmed))
+        threads_list.append(worker)
+        worker.start()
+    for thread in threads_list:
+        thread.join()
+    print(f"[+] Job completed in {time.time()-timestamp_start} seconds.")
+    if len(confirmed) > 0:
+        print("[+] The following virtualhost were discovered:")
+        for site in confirmed:
+            print(f"* {site}")
+    else:
+        print("[-] No virtualhosts were discovered.")
+
+
+if __name__ == '__main__':
+    main()
+
